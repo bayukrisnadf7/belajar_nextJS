0651483c8947ee8a221bc6b410e9870a38036d3d
bayukrisna <bayukrisna14658@gmail.com>
2024-07-31 02:15:22 +0700

    belajar client side rendering

---
+++ src/views/Product/Main/index.tsx
@@ -0,0 +1,35 @@
+import styles from "./Product.module.scss";
+
+type typeProduct = {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+};
+
+const ProductView = ({ products }: { products: typeProduct[] }) => {
+    console.log(products)
+  return (
+    <>
+      <div className={styles.product}>
+        <h1 className={styles.product__title}>Product Page</h1>
+        <div className={styles.product__content}>
+          {products.map((product: typeProduct) => (
+            <div key={product.id} className={styles.product__content__item}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className={styles.product__content__item__image}
+              ></img>
+              <h1 className={styles.product__content__item__name}>{product.name}</h1>
+              <p className={styles.product__content__item__category}>{product.category}</p>
+              <p className={styles.product__content__item__price}>{product.price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+export default ProductView;
